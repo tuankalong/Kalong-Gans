84ed393dbfae75a3203fcf03991da0d358bcc02c
tuankalong <44815236+tuankalong@users.noreply.github.com>
2018-11-09 19:27:28 +0000

    Add files via upload

---
+++ Crazy.sh
@@ -0,0 +1,285 @@
+#Mau ngapai lu :v
+
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+#1
+clear
+echo
+echo $red
+echo "L"
+sleep 0.1
+clear
+echo $red
+echo "LO"
+sleep 0.1
+clear
+echo $red
+echo "LOA"
+sleep 0.1
+clear
+echo $red
+echo "LOAD"
+sleep 0.1
+clear
+echo $white
+echo "LOADI"
+sleep 0.1
+clear
+echo $white
+echo "LOADIN"
+sleep 0.1
+clear
+echo $white
+echo "LOADING"
+sleep 1
+clear
+echo $red
+echo "L"
+sleep 0.1
+clear
+echo $red
+echo "LO"
+sleep 0.1
+clear
+echo $red
+echo "LOA"
+sleep 0.1
+clear
+echo $red
+echo "LOAD"
+sleep 0.1
+clear
+echo $white
+echo "LOADI"
+sleep 0.1
+clear
+echo $white
+echo "LOADIN"
+sleep 0.1
+clear
+echo $white
+echo "LOADING"
+sleep 1
+clear
+echo $cyan
+echo "Sabar njirrr :v"
+sleep 3
+clear
+echo $red
+echo "L"
+sleep 0.1
+clear
+echo $red
+echo "LO"
+sleep 0.1
+clear
+echo $red
+echo "LOA"
+sleep 0.1
+clear
+echo $red
+echo "LOAD"
+sleep 0.1
+clear
+echo $white
+echo "LOADI"
+sleep 0.1
+clear
+echo $white
+echo "LOADIN"
+sleep 0.1
+clear
+echo $white
+echo "LOADING"
+sleep 0.5
+clear
+
+#2
+
+echo
+figlet Tn. Kalong |lolcat
+echo $cyan "+*+*+*+*+*+*+*+*+*+*"$white WELLCOM $cyan"+*+*+*+*+*+*+*+*+*+*"
+echo $cyan "*  $red Author : Tn .Kalong                         +"
+echo $cyan "+ $red Contact : 081269185159                       *"
+echo $cyan "+*+*+*+*+*+*+*+*+*+*"$white °Crazy° $cyan"+*+*+*+*+*+*+*+*+*+*"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white" + [ 01 ]"$red"LITESPAM             +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white" + [ 02 ]"$red"Lazymux              +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white "+ [ 03 ]"$red"LITESCRIPT           +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white "+ [ 04 ]"$red"Tool-X               +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white "+ [ 05 ]"$red"VbugMaker Virus      +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white "+ [ 06 ]"$red"Red_Hawk             +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white "+ [ 07 ]"$red"admin-Finder         +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white "+ [ 08 ]"$red"spam bom-mail        +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white "+ [ 09 ]"$red"iesDEFACE            +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white "+ [ 10 ]"$red"Hammer               +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white "+ [ 11 ]"$red"Gado-Gado            +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white "+ [ 12 ]"$red"webpwn3r             +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white "+ [ 13 ]"$red"termux_webscan       +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white "+ [ 14 ]"$red"tools diejoubu       +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white "+ [ 15 ]"$red"ko-dork              +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $white "+ [ 00 ] info"$red  "[ 99 ] Exit    +"
+echo $indigo"++++++++++++++++++++++++++++++++++++"
+echo $cyan
+read -p " root@Gans;~ " pea ;
+
+if [ $pea = 01 ] || [ $pea = 1 ]
+then
+cd files
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pea = 02 ] || [ $pea = 2 ]
+then
+cd files
+cd Lazymux
+python2 lazymux.py
+fi
+
+if [ $pea = 03 ] || [ $pea = 3 ]
+then
+cd files
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+
+if [ $pea = 04 ] || [ $pea = 4 ]
+then
+cd files
+cd Tool-X
+chmod +x install.aex
+sh install.aex
+fi
+
+if [ $pea = 05 ] || [ $pea = 5 ]
+then
+cd files
+cd vbug
+python2 vbug.py
+fi
+
+if [ $pea = 06 ] || [ $pea = 6 ]
+then
+cd files
+cd RED_HAWK
+php rhawk.php
+fi
+
+if [ $pea = 07 ] || [ $pea = 7 ]
+then
+cd files
+cd admin-finder
+python2 admin-finder.py
+fi
+
+if [ $pea = 08 ] || [ $pea = 8 ]
+then
+cd files
+cd bom-mail
+python2 Bom-mail.py
+fi
+
+if [ $pea = 09 ] || [ $pea = 9 ]
+then
+cd files
+cd iesDEFACE
+bash iesDeface.sh
+fi
+
+if [ $pea = 10 ] || [ $pea = 10 ]
+then
+cd files
+cd hammer
+python hammer.py
+fi
+
+if [ $pea = 11 ] || [ $pea = 11 ]
+then
+cd files
+cd GadoGado
+bash gado
+fi
+
+if [ $pea = 12 ] || [ $pea = 12 ]
+then
+cd files
+cd webpwn3r
+chmod +x scan.py
+python2 scan.py
+fi
+
+if [ $pea = 13 ] || [ $pea = 13 ]
+then
+cd files
+cd termux_webscan
+Sh install.sh
+./webscan
+fi
+
+
+if [ $pea = 14 ] || [ $pea = 14 ]
+then
+cd files
+cd diejoubu
+cd v1.2
+php daijobu.php
+fi
+
+if [ $pea = 15 ] || [ $pea = 15 ]
+then
+cd files
+cd ko-dork
+python2 dork.py
+fi
+
+if [ $pea = 00 ] || [ $pea = 00 ]
+then
+pkg install toilet -y
+clear
+toilet -f future "Tn. Kalong"
+echo $cyan "+*+*+*+*+*+*+*+*+*+*"$white WELLCOM $cyan"+*+*+*+*+*+*+*+*+*+*"
+echo $cyan "*  $red Author : Tn. Kalong                         +"
+echo $cyan "+ $red Contact : 081269185159                       *"
+echo $cyan "+*+*+*+*+*+*+*+*+*+*"$white °Crazy° $cyan"+*+*+*+*+*+*+*+*+*+*"
+echo $red "[ 000 ] back"
+echo $red "[ 99 ] exit"
+echo $cyan
+read -p " root@KmGans;~ " pea
+fi
+
+if [ $pea = 99 ] || [ $pea = 99 ]
+then
+echo $cyan "exit...."
+fi
+
+if [ $pea = 000 ] || [ $pea = 000 ]
+then
+sh Crazy.sh
+fi
+
+if [ $pea = install ] || [ $pea = install ]
+then
+cd files
+sh install.sh
+fi
